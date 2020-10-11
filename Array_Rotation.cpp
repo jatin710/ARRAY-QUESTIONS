a4b79bbf66585b626b02bbe8a576901a2e30452c
jatin710 <47669092+jatin710@users.noreply.github.com>
2020-10-11 16:29:50 +0530

    Add files via upload
    Left array Rotation program

---
+++ Array_Rotation.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#define max 100
+using namespace std;
+  int gcd(int a,int b)
+  {
+      if(b ==0)
+      return a;
+      else
+         return gcd(b,a%b);
+  }
+
+ void arr_rot(int *a,int n, int d)
+ {  
+     int g_c_d,i,j,k,temp;
+     d = d%n;
+     g_c_d=gcd(n,d);
+    for( i = 0 ; i<g_c_d;i++)
+    {
+        temp =  a[i];
+        j =i;
+        while(1)
+        {
+            k = j+d;
+            if(k >= n)
+            k = k- n;
+            if( k == i)
+              break;
+            else
+              
+              
+              a[j] = a[k];
+                j = k;
+        }
+        a[j] = temp;
+    }
+     
+ }
+  void print( int *a , int n)
+  {
+      int i;
+      for(i = 0 ; i < n ; i++)
+      {
+          cout<<a[i]<<" ";
+      }
+  }
+int main() {
+     int a[max], d,n,i;
+     cin>>n;
+     for(i = 0 ; i < n; i++)
+     {
+         cin>>a[i];
+     }
+     cin>>d;
+     arr_rot(a,n,d);
+     print(a,n);
+	return 0;
+}
